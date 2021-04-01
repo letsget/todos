961de99e8613b0d8580fd7059815a5e3ae1aa43f
Andrey Bondarev <andrewbond25@gmail.com>
2021-04-01 21:54:16 +0200

    added redux-persist for storing todos in local storage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { appReducer } from "./appReducer";
+
+const persistConfig = {
+  key: "todos",
+  storage,
+  whitelist: ["app"],
+};
 
 const rootReducer = combineReducers({
   app: appReducer,
 });
 
-export { rootReducer };
+export default persistReducer(persistConfig, rootReducer);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 // import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-
-import { rootReducer } from "./reducers";
+import { persistStore } from "redux-persist";
+import rootReducer from "./reducers";
 
 const store = createStore(rootReducer);
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
